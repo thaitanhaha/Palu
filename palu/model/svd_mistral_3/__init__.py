2df1475a2427ad3051c140939b3a069fe2b0e799
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2026-06-24 11:23:47 +0700

    fix configs

--- palu/model/svd_mistral_3/__init__.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer
-from .configuration_palu_mistral_3 import PaluMistral3Config
-from .modeling_palu_mistral_3 import PaluMistral3ForCausalLM
-
-AutoConfig.register("palumistral3", PaluMistral3Config)
-AutoModelForCausalLM.register(PaluMistral3Config, PaluMistral3ForCausalLM)
-AutoTokenizer.register(PaluMistral3Config, LlamaTokenizer)
-
